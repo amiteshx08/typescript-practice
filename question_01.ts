f53e54e71bfc50888d0ed7c63642b84a9e91dccb
Amitesh Harsh Lal <amiteshharshlal@gmail.com>
2026-01-21 00:02:07 +0530

    feat: Solved factorial of a number

---
+++ question_01.ts
@@ -0,0 +1,12 @@
+// factorial of a number question
+
+function factorial(n: number): number{
+    if(n < 0 || !Number.isInteger(n) || Number.isNaN(n)) throw new Error('Invalid Input')
+
+    if(n <= 1) return 1
+    
+    return factorial(n - 1) * n
+}
+
+console.log(factorial(3))
+export {}
